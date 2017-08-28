144004e68488fe334a5f86de09b966a9d0052917
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2017-08-29 09:42:06 +1000

    Update build.sh with correct target frameworks

--- build.sh
+++ build.sh
@@ -5,15 +5,13 @@ dotnet --info
 dotnet restore
 
 for path in src/**/*.csproj; do
-    dotnet build -f netstandard1.3 -c Release ${path}
-    dotnet build -f netcoreapp1.1 -c Release ${path}
+    dotnet build -f netstandard1.0 -c Release ${path}
 done
 
 for path in test/*.Tests/*.csproj; do
-    dotnet test -f netcoreapp1.1  -c Release ${path}
+    dotnet test -f netcoreapp2.0  -c Release ${path}
 done
 
-for path in sample/ConsoleDemo/*.csproj; do
-    dotnet build -f netcoreapp1.1 -c Release ${path}
-    dotnet run -f netcoreapp1.1 --project ${path}
+for path in sample/**/*.csproj; do
+    dotnet build -f netcoreapp2.0 -c Release ${path}
 done
